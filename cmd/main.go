7bfbab08deb968139797ae5761d7cc754e2f7291
Constantin <kingpoker1@mail.ru>
2021-02-07 15:07:34 +0300

    Dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,14 @@ import (
 
 	todo "github.com/Constantin81/Todo-app"
 	"github.com/Constantin81/Todo-app/pkg/handler"
-	// "github.com/Constantin81/Todo-app/pkg/repository"
-	// "github.com/Constantin81/Todo-app/pkg/service"
+	"github.com/Constantin81/Todo-app/pkg/repository"
+	"github.com/Constantin81/Todo-app/pkg/service"
 )
 
 func main() {
-	// repos := new(repository.NewRepository())
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s ", err.Error())
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,10 +1,16 @@
 package handler
 
 import (
+	"github.com/Constantin81/Todo-app/pkg/service"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
